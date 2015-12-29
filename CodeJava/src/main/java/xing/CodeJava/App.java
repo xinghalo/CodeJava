4373ebd1d6a28f46a66c4b8223d7b6bd6103d8e1
xinghalo <xinghalo@163.com>
2015-12-29 16:46:05 +0800

    Boolean

---
+++ CodeJava/src/main/java/xing/CodeJava/App.java
@@ -0,0 +1,13 @@
+package xing.CodeJava;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
