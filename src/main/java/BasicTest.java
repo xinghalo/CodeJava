fd48b1d74c67e21c0685cbf4aba8fafbdf7d31ca
xinghalo <xinghalo@163.com>
2016-05-10 22:25:35 +0800

    change dir

---
+++ src/main/java/BasicTest.java
@@ -0,0 +1,17 @@
+/**
+ * Test for javadoc
+ * @author xing.hl
+ *
+ */
+public class BasicTest {
+	/**
+	 * this is test
+	 */
+	public void test(){
+		System.out.println("test()");
+	}
+	public static void main(String[] args) {
+		BasicTest test = new BasicTest();
+		test.test();
+	}
+}
