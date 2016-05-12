80efa40f0e653e328fa8e16da7054375a691360d
xinghalo <xinghalo@163.com>
2016-05-12 22:44:25 +0800

    《编程思想》第五章

---
+++ src/xing/test/thinking/chap5/InitialValues.java
@@ -0,0 +1,29 @@
+package xing.test.thinking.chap5;
+
+public class InitialValues {
+	boolean t;
+	char c;
+	byte b;
+	short s;
+	int i;
+	long l;
+	float f;
+	double d;
+	InitialValues reference;
+	void print(){
+		System.out.println("initial values");
+		System.out.println("boolean: "+t);
+		System.out.println("char: "+c);
+		System.out.println("byte: "+b);
+		System.out.println("short: "+s);
+		System.out.println("int: "+i);
+		System.out.println("long: "+l);
+		System.out.println("float: "+f);
+		System.out.println("double: "+d);
+		System.out.println("reference: "+reference);
+	}
+	public static void main(String[] args) {
+		InitialValues init = new InitialValues();
+		init.print();
+	}
+}
---
+++ src/xing/test/thinking/chap5/TerninationCondition.java
@@ -0,0 +1,27 @@
+package xing.test.thinking.chap5;
+
+class Book {
+	boolean chechedOut = false;
+	public Book(boolean checkout) {
+		this.chechedOut = checkout;
+	}
+	void checkIn(){
+		this.chechedOut = false;
+	}
+	protected void finalize(){
+		if(chechedOut)
+			System.out.println("Error: checked out");
+//		super.finalize();
+	}
+}
+public class TerninationCondition {
+	public static void main(String[] args) {
+		Book book1 = new Book(true);
+		book1.checkIn();
+		book1 = null;
+		System.gc();
+		System.out.println("-----------------------");
+		new Book(true);
+		System.gc();
+	}
+}
