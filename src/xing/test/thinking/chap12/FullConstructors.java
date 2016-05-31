ef36d131e647a08e41949822184e68f0346adf25
xinghalo <xinghalo@163.com>
2016-05-31 22:44:14 +0800

    《编程思想》—— 异常

---
+++ src/xing/test/thinking/chap12/FullConstructors.java
@@ -0,0 +1,27 @@
+package xing.test.thinking.chap12;
+class MyException extends Exception{
+	public MyException(){}
+	public MyException(String msg){super(msg);}
+}
+public class FullConstructors {
+	public static void f() throws MyException{
+		System.out.println("Throwing MyException from f()");
+		throw new MyException();
+	}
+	public static void g() throws MyException{
+		System.out.println("Throwing MyException from f()");
+		throw new MyException("Originated in g()");
+	}
+	public static void main(String[] args) {
+		try{
+			f();
+		}catch(MyException e){
+			e.printStackTrace(System.err);
+		}
+		try{
+			g();
+		}catch(MyException e){
+			e.printStackTrace(System.out);
+		}
+	}
+}
---
+++ src/xing/test/thinking/chap12/InheritingExceptions.java
@@ -0,0 +1,17 @@
+package xing.test.thinking.chap12;
+class SimpleException extends Exception{}
+public class InheritingExceptions {
+	public void f() throws SimpleException{
+		System.out.println("Throw SimpleException from f()");
+		throw new SimpleException();
+	}
+	public static void main(String[] args) {
+		InheritingExceptions sed = new InheritingExceptions();
+		try{
+			sed.f();
+		}catch(SimpleException e){
+			System.out.println("Caught it!");
+			e.printStackTrace();
+		}
+	}
+}
